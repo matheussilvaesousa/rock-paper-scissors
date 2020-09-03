dbdce4223071e369cea5ce29f35bc0098ae69269
Matheus Sousa <matheussilvaesousa@gmail.com>
2020-09-02 21:21:11 -0300

    Add basic structure including JS blueprint

---
+++ app.js
@@ -0,0 +1,24 @@
+function game() {
+
+    for(let i = 0; i < 5; i++) {
+
+        //Case insensitive user input
+        let playerSelection = input()
+        let computerSelection = computerPlay()
+
+        // Returns either rock, paper or scissors
+        function computerPlay() {
+
+        }
+
+        // Plays a single round, return the winner of the round
+        function playRound(playerSelection, computerSelection) {
+
+        }
+
+        console.log(playRound())
+    }
+
+}
+
+game()
